861b0e971967a7b0ac5974181e58126c97226c55
Alireza Rahmani Samani <alir.rah81@gmail.com>
2025-02-22 16:19:15 +0330

    [ADD] change theme button

--- dream-hunter/src/components/Toolbar/Toolbar.tsx
+++ dream-hunter/src/components/Toolbar/Toolbar.tsx
@@ -2,16 +2,22 @@ import { ReactNode } from "react";
 
 import TextInput from "../TextInput/TextInput.tsx";
 import Select from "../Select/Select.tsx";
+import Button from "../Button/Button.tsx";
 
 import MingcuteSearch3Line from "../../icons/MingcuteSearch3Line.tsx";
+import MingcuteMoonLine from "../../icons/MingcuteMoonLine.tsx";
+import MingcuteSunLine from "../../icons/MingcuteSunLine.tsx";
 
 import styles from "./Toolbar.module.css";
 
 function Toolbar(): ReactNode {
+  const theme = "light";
+
   return (
     <div className={styles.toolbar}>
       <TextInput
         placeholder="Search Dream..."
+        className={styles.input}
         suffixIcon={<MingcuteSearch3Line />}
       />
       <Select
@@ -21,6 +27,9 @@ function Toolbar(): ReactNode {
           { value: "bad", label: "Bad" },
         ]}
       />
+      <Button variant="solid" size="medium" shape="square">
+        {theme === "light" ? <MingcuteMoonLine /> : <MingcuteSunLine />}
+      </Button>
     </div>
   );
 }
